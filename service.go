229eff705d5214d47fec77c803a7e8f4a4895b50
Tony Yip <tony@opensource.hk>
2018-05-08 05:47:47 +0800

    Initial commit

---
+++ service.go
@@ -0,0 +1,81 @@
+package totp
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+//go:generate protoc -I . --go_out=plugins=grpc:. ./service.proto
+
+type Client struct {
+	Endpoint string
+}
+
+func (c *Client) connect(ctx context.Context) (*grpc.ClientConn, error) {
+	return grpc.DialContext(ctx, c.Endpoint, grpc.WithInsecure())
+}
+
+func (c *Client) IssueKey(ctx context.Context, in *IssueKeyRequest, opts ...grpc.CallOption) (*IssueKeyReply, error) {
+	cc, err := c.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cc.Close()
+
+	out := new(IssueKeyReply)
+	err = cc.Invoke(ctx, "/totp.Totp/issueKey", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *Client) Validate(ctx context.Context, in *ValidateRequest, opts ...grpc.CallOption) (*ValidateReply, error) {
+	cc, err := c.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cc.Close()
+
+	out := new(ValidateReply)
+	err = cc.Invoke(ctx, "/totp.Totp/validate", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *Client) RecoverKey(ctx context.Context, in *RecoverRequest, opts ...grpc.CallOption) (*RecoverReply, error) {
+	cc, err := c.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cc.Close()
+
+	out := new(RecoverReply)
+	err = cc.Invoke(ctx, "/totp.Totp/recoverKey", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *Client) RemoveKey(ctx context.Context, in *RemoveKeyRequest, opts ...grpc.CallOption) (*RemoveKeyReply, error) {
+	cc, err := c.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cc.Close()
+
+	out := new(RemoveKeyReply)
+	err = cc.Invoke(ctx, "/totp.Totp/removeKey", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
